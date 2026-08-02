b7efedb33b4e9178c8cf0e9c3387ec602aaab2a5
Farhan8271git <farhanali143865@gmail.com>
2026-08-02 22:52:14 +0530

    refactor(core): standardize API responses and prepare authentication module

---
+++ server/src/models/user.model.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: [true, "Full name is required"],
+      trim: true,
+      minlength: 3,
+      maxlength: 100,
+    },
+
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+
+    accountStatus: {
+      type: String,
+      enum: ["active", "suspended", "blocked"],
+      default: "active",
+    },
+
+    storageUsed: {
+      type: Number,
+      default: 0,
+    },
+
+    storageLimit: {
+      type: Number,
+      default: 5 * 1024 * 1024 * 1024,
+    },
+
+    lastLogin: {
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
